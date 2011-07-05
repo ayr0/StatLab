af41d62862cbf441e55580da90c08e5420f57187
m4webb <we13ster@gmail.com>
2011-07-05 11:50:44 -0700

    New stats

--- statlab.py
+++ statlab.py
@@ -1,48 +1,42 @@
 import scipy.stats as st
 import scipy as sp
 import scipy.linalg as la
+import matplotlib.pyplot as plt
 
 class StatLab(object):
-    def __init__(self, response, ivars):
-        self.response = Y
-        self.ivars = X
-        
 
-    def MLE(self):
-        r"""
-        Calculate the Maximum Likelyhood Estimation
-        """
-        pass
+	def __init__(self, X, Y):
+		self.response = Y
+		self.ivars = X
+		self.Xdim = int(X.shape[1]) #Number of progressors plus one
+		self.ndim = int(X.shape[0]) #Number of trials
+		self.beta_hat = la.inv(X.T.dot(X)).dot(X.T.dot(Y))
+		self.projection = X.dot(self.beta_hat)
+		self.residuals = Y - self.projection
+		self.SSE = self.residuals.T.dot(self.residuals)
+		self.MSE = self.SSE/(self.ndim - self.Xdim)
+		self.variance = la.inv(self.MSE*X.T.dot(X))
+		self.hypothesis = sp.zeros(self.Xdim)
+	
+	def test_stats(self):
+		tstats = sp.zeros(self.Xdim)
+		for x in range(self.Xdim):
+			tstats[x] = (self.beta_hat[x]-self.hypothesis[x])/(self.MSE*sp.sqrt(self.variance[x,x]))
+		return tstats
+		
+	def dec_rule(self, alpha=0.05):
+		return st.t.ppf(1-alpha/2.0, self.ndim-self.Xdim)
+		
+	def pvals(self, alpha=0.05):
+		return 2*(1-st.t.cdf(self.test_stats(), self.ndim-self.Xdim))
+		
+	def analyze(self, alpha=0.05):
+		print ""
+		print "    Decision rule: %.5f" %self.dec_rule()
+		print "    beta             dev              tstats           pvals"
+		print sp.vstack((self.beta_hat.T,sp.sqrt(sp.diag(self.variance)), self.test_stats(), self.pvals())).T
+		return 
 
-    def MSE(self):
-        """Calculate the MSE"""
-
-        k = min(self.X.shape)-1
-        n = max(self.Y.shape)
-
-        
-
-
-        H = X.dot(la.inv(X.T.dot(X)).dot(X.T))
-        yhy=Y.T.dot((sp.eye(H.shape[0])-H).dot(Y))
-        
-        if yhy.shape == (1,n):
-            return (1.0/(n-k-1))*sp.dot(yhy, yhy.T)
-        else:
-            return (1.0/(n-k-1))*sp.dot(yhy.T, yhy)
-
-    def _SSE(self):
-        fy = beta_hat().dot(self.X)
-        return (self.Y-fy).T.dot(self.Y-fy)
-        
-    def beta_hat(self):
-        return self._beta_hat(self.ivars, self.response)
-        
-    def _test_statistics(self, beta_hat, beta):
-        return B[bvar,0]/float(covar[bvar,bvar])
-    
-    def matt():
-        """"Matt was here"""
-        pass
-
-        
+	def plotX(self, n):
+		plt.plot(self.ivars[:,n],self.response,'o')
+		plt.show()
