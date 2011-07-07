04abfaff066c6cbd3e5fcf7a3396ec25aab2b80d
Ryan Grout <rgrout@byu.net>
2011-07-07 17:09:59 -0600

    fixed formatting

--- statlab.py
+++ statlab.py
@@ -10,7 +10,7 @@ class StatLab(object):
 		self.ivars = X
 		self.alpha = 0.05
 		self.calc()
-		
+			
 	def calc(self):
 		self.Xdim = self.ivars.shape[1] #Number of progressors plus one
 		self.ndim = self.ivars.shape[0] #Number of trials
@@ -31,13 +31,13 @@ class StatLab(object):
 			self.tstats[x] = (self.beta_hat[x]-self.hypothesis[x])/(self.MSE*sp.math.sqrt(self.variance[x,x]))
 		
 		
-		self.drule=st.t.ppf(1-self.alpha/2.0, self.ndim-self.Xdim)
+		self.drule=st.t.ppf(1-(self.alpha/2.0), self.ndim-self.Xdim)
 		self.pvals = 2.0*(1-st.t.cdf(abs(self.tstats), self.ndim-self.Xdim))
 	
 	def analyze(self):
 		print "    Alpha: %.3f" % self.alpha
 		print "    Decision rule: %.5f" %self.drule
-		print "    beta             dev              tstats           pvals"
+		print "    Beta             Dev              T-stats           P-vals"
 		print sp.vstack((self.beta_hat.T,sp.sqrt(sp.diag(self.variance)), self.tstats, self.pvals)).T
 		return 
 		
@@ -45,8 +45,12 @@ class StatLab(object):
 		plt.plot(self.ivars[:,n],self.response,'o')
 		plt.show()
 
-	def confInt(self, n):
-		x = sp.math.sqrt(self.variance[n,n])*self.drule
-		lowBound = self.beta_hat[n] - x
-		upBound = self.beta_hat[n] + x
-		print "The %d%% confidence interval for X%d is from %f.5 to %f.5" %((1-self.alpha)*100, n, lowBound, upBound)
+	def confInt(self, n, confidence=None):
+		if confidence is not None:
+			print "For %d%% confidence of X%d, alpha needs to be %.3f" % (confidence*100.0, n, 1.0-confidence)
+			return 1.0-confidence
+		else:	 
+			x = sp.math.sqrt(self.variance[n,n])*self.drule
+			lowBound = self.beta_hat[n] - x
+			upBound = self.beta_hat[n] + x
+			print "The %d%% confidence interval for X%d is from %f.5 to %f.5" %((1-self.alpha)*100, n, lowBound, upBound)
